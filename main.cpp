fba68c7c5f926e26e93838878d401a0258335999
Greg Romane <gregromane@gmail.com>
2015-08-20 22:53:29 -0700

    Update main.cpp to use the filehandler routines as a plug-in

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include "filehandler.h"
+
+
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    qmlRegisterSingletonType<fileHandler>("FileHandler", 1, 0, "FileHandler", fileHandlerCallBack);
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
